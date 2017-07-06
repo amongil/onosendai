8a9344afc68d7050dfcff2f7053f5d6577862e8b
amongil <almonfe1@gmail.com>
2017-07-06 22:20:20 +0200

    Update scan cmd

--- onosendai/cmd/scan.go
+++ onosendai/cmd/scan.go
@@ -25,11 +25,15 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
 var server string
+var idFilename string
+var idFile string
 
 // scanCmd represents the scan command
 var scanCmd = &cobra.Command{
@@ -39,16 +43,16 @@ var scanCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		if server != "" {
 			fmt.Printf("Connecting to server <%s>\n", server)
-			url := server + "/"
-			res, err := scanRequest("GET", url)
+			url := server + "/fingerprint"
+			res, err := scanRequest("POST", url, idFilename)
 			if err != nil {
 				fmt.Printf("Error occurred: %s\n", err.Error())
 			} else {
 				fmt.Println(res)
 			}
 		} else {
-			fmt.Printf("Usage error: When using the scan command, you must specify a server.\n\n")
-			fmt.Println("Example usage: onosendai scan -server blackice.maaslabs.com")
+			fmt.Printf("Usage error: When using the scan command, you must specify a server and an identity file.\n\n")
+			fmt.Println("Example usage: onosendai scan -s blackice.maaslabs.com -i ~/.ssh/icebreaker.pem")
 		}
 	},
 }
@@ -66,22 +70,39 @@ func init() {
 	// is called directly, e.g.:
 	//scanCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 	scanCmd.Flags().StringVarP(&server, "server", "s", "", "blackice Server to connect to")
+	scanCmd.Flags().StringVarP(&idFilename, "identity_file", "i", "", "your identity file")
 }
 
-func scanRequest(method string, url string) (string, error) {
-	// Build the request
-	req, err := http.NewRequest("GET", url, nil)
+func scanRequest(method string, APIURL string, idFilename string) (string, error) {
+	// For control over HTTP client headers, redirect policy, and other settings
+	client := &http.Client{}
+
+	// Read our identity file and store it
+	identity, err := ioutil.ReadFile(idFilename)
+	if err != nil {
+		return "", err
+	}
+
+	// Add our form where the ssh key will be sent on
+	form := url.Values{}
+	form.Add("identity", string(identity))
+
+	// Form our http request
+	req, err := http.NewRequest("POST", APIURL, strings.NewReader(form.Encode()))
 	if err != nil {
 		log.Fatal("NewRequest: ", err)
 		return "", err
 	}
-	// For control over HTTP client headers, redirect policy, and other settings
-	client := &http.Client{}
+	req.PostForm = form
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	// Print it for logging
+	//fmt.Println(formatRequest(req))
 
 	// Send the request via a client
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Fatal("Do: ", err)
+		log.Fatal("Error sending the request: ", err.Error())
 		return "", err
 	}
 	defer resp.Body.Close()
@@ -89,4 +110,31 @@ func scanRequest(method string, url string) (string, error) {
 	bodyBytes, err := ioutil.ReadAll(resp.Body)
 	bodyString := string(bodyBytes)
 	return bodyString, err
+}
+
+// formatRequest generates ascii representation of a request
+func formatRequest(r *http.Request) string {
+	// Create return string
+	var request []string
+	// Add the request string
+	url := fmt.Sprintf("%v %v %v", r.Method, r.URL, r.Proto)
+	request = append(request, url)
+	// Add the host
+	request = append(request, fmt.Sprintf("Host: %v", r.Host))
+	// Loop through headers
+	for name, headers := range r.Header {
+		name = strings.ToLower(name)
+		for _, h := range headers {
+			request = append(request, fmt.Sprintf("%v: %v", name, h))
+		}
+	}
+
+	// If this is a POST, add post data
+	if r.Method == "POST" {
+		r.ParseForm()
+		request = append(request, "\n")
+		request = append(request, r.Form.Encode())
+	}
+	// Return the request as a string
+	return strings.Join(request, "\n")
 }
